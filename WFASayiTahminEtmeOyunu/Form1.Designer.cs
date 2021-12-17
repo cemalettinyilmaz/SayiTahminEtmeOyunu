1e8c5891d943fff73fb3bea28394707673332f77
Cemalettin <Cemalettin@CEMO>
2021-12-17 21:42:46 +0300

    Add project files.

---
+++ WFASayiTahminEtmeOyunu/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace WFASayiTahminEtmeOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPuan = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblIpucu = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblSaniye = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 224);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(219, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Puanınız";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblPuan
+            // 
+            this.lblPuan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPuan.Location = new System.Drawing.Point(219, 52);
+            this.lblPuan.Name = "lblPuan";
+            this.lblPuan.Size = new System.Drawing.Size(100, 20);
+            this.lblPuan.TabIndex = 0;
+            this.lblPuan.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(219, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Sayı Girişi Yapınız";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(219, 185);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(201, 26);
+            this.txtSayi.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tahmin Et!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(219, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "İpucu !";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblIpucu
+            // 
+            this.lblIpucu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblIpucu.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblIpucu.Location = new System.Drawing.Point(219, 112);
+            this.lblIpucu.Name = "lblIpucu";
+            this.lblIpucu.Size = new System.Drawing.Size(201, 42);
+            this.lblIpucu.TabIndex = 0;
+            this.lblIpucu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblSaniye
+            // 
+            this.lblSaniye.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSaniye.Location = new System.Drawing.Point(325, 52);
+            this.lblSaniye.Name = "lblSaniye";
+            this.lblSaniye.Size = new System.Drawing.Size(95, 20);
+            this.lblSaniye.TabIndex = 3;
+            this.lblSaniye.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(325, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Kalan Süre";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 267);
+            this.Controls.Add(this.lblSaniye);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.lblPuan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblIpucu);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Sayı Tahmin Etme Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.Label label1;
+        protected System.Windows.Forms.Label label2;
+        protected System.Windows.Forms.Label lblPuan;
+        protected System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Button button1;
+        protected System.Windows.Forms.Label label4;
+        protected System.Windows.Forms.Label lblIpucu;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblSaniye;
+        protected System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WFASayiTahminEtmeOyunu/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFASayiTahminEtmeOyunu
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            lblPuan.Text = puan.ToString();
+
+            lblIpucu.Text = "Sayı 0 ile 1000 arasındadır";
+            sayi = rnd.Next(1001);
+            lblSaniye.Text = "60";
+
+        }
+
+        int puan = 1000;
+        Random rnd = new Random();
+        int sayi;
+        int sayac = 0;
+        int saniye = 60;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+
+            saniye = saniye - 1;
+            lblSaniye.Text = Convert.ToString(saniye);
+            if (saniye == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Süren Bitti Oyun Bitti !", "Süre bitti gitti!");
+                DialogResult dialogResult2 = MessageBox.Show("Yeni oyuna başlamak ister misin ?", "Tekrar ?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialogResult2 == DialogResult.Yes)
+                {
+                    MessageBox.Show("Sayıyı çoktan değiştirdim. Kaldığın yerden devam etmeyi deneme ! ", "Haberin olsun !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    puan = 1000;
+                    sayi = rnd.Next(1001);
+                    sayac = 0;
+                    saniye = 60;
+                    lblSaniye.Text = Convert.ToString(saniye);
+                }
+                else
+                    Close();
+
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                timer1.Start();
+
+                int tahmin = Convert.ToInt32(txtSayi.Text);
+                
+
+                if (sayi == tahmin)
+                {
+                    timer1.Stop();
+                    lblIpucu.Text = $"Tebrikler kazandınız";
+                    MessageBox.Show("TEBRİKLER OYUNU KAZANDINIZ", "KAZANDINIZ !!!!!");
+                    DialogResult dialogResult = MessageBox.Show("Yeniden Oynamak İstermisiniz ?", "Yeniden ?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        
+                        MessageBox.Show("Sayı tekrar değiştirildi bol şans. ", "Haberin olsun !", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        puan = 1000;
+                        sayi = rnd.Next(1001);
+                        saniye = 60;
+                        lblSaniye.Text = Convert.ToString(saniye);
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        Close();
+
+                    }
+                }
+                else if (tahmin < sayi)
+                {
+                    lblIpucu.Text = $"Girdiğin sayı :{tahmin} \r\n Tahminini arttır.";
+                    puan -= 100;
+                }
+                else if (tahmin > sayi)
+                {
+                    lblIpucu.Text = $"Girdiğin sayı :{tahmin} \r\n Tahminini düşür.";
+                    puan -= 100;
+                }
+
+                if (puan == 0 || puan < 0)
+                {
+                    DialogResult dialogResult = MessageBox.Show("Puanınız tükendi 500 puan daha verebilirim.İster misin ?", "Kaybettin", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        if (sayac == 0)
+                        {
+                            MessageBox.Show("2.Şansın iyi kullan 500 puan daha verdim.", "Haberin olsun !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            puan = 500;
+                            sayac++;
+                        }
+                        else
+                        {
+                            timer1.Stop();
+                            MessageBox.Show("2.Şans bir kere gelir üzgünüm Oyun Bitti.", "Geçmiş olsun!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            Close();
+                        }
+                    }
+
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        timer1.Stop();
+                        DialogResult dialogResult2 = MessageBox.Show("Yeni oyuna başlamak ister misin ?", "Tekrar ?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (dialogResult2 == DialogResult.Yes)
+                        {
+                            
+                            MessageBox.Show("Sayıyı çoktan değiştirdim. Kaldığın yerden devam etmeyi deneme ! ", "Haberin olsun !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            puan = 1000;
+                            sayi = rnd.Next(1001);
+                            sayac = 0;
+                            saniye = 60;
+                            lblSaniye.Text = Convert.ToString(saniye);
+                        }
+                        else
+                            Close();
+                    }
+                    else if (dialogResult == DialogResult.Cancel)
+                        Close();
+
+                }
+                
+                
+
+                lblPuan.Text = puan.ToString();
+                txtSayi.Clear();
+
+
+            }
+
+            catch
+            {
+                MessageBox.Show("Sadece sayı girişi yapabilirsin", "Hop Nereye ?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+
+        }
+
+      
+
+       
+        
+    }
+}
